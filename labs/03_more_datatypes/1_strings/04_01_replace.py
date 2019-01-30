57a92aaea19ae046b1d263b4642b1468e9df92e8
martin-martin <breuss.martin@gmail.com>
2019-01-30 12:43:43 +0700

    one lab down

--- labs/03_more_datatypes/1_strings/04_01_replace.py
+++ labs/03_more_datatypes/1_strings/04_01_replace.py
@@ -8,3 +8,11 @@ Result: #ore python progra##ing please
 
 '''
 
+# get user input (sentence)
+sentence = input("Please enter a sentence: ")
+# get user input (symbol)
+symbol = input("Please enter a symbol: ")
+# get first letter of sentence
+first = sentence[0]
+# replace all occurences of first letter with symbol
+print(sentence.replace(first, symbol))
